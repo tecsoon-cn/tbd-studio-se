e51591913a361378b2e9e1d4352f4c612f4bb8aa
msjian <msjian@talend.com>
2021-08-19 16:48:43 +0800

    fix(TDQ-19386): fix support spark3 for tMatchPredict

--- main/plugins/org.talend.hadoop.distribution.spl30x/src/main/java/org/talend/hadoop/distribution/spl30x/SPL30xDistribution.java
+++ main/plugins/org.talend.hadoop.distribution.spl30x/src/main/java/org/talend/hadoop/distribution/spl30x/SPL30xDistribution.java
@@ -122,6 +122,12 @@ public class SPL30xDistribution extends AbstractDistribution
                                 .getModuleGroup(ModuleGroupName.PARQUET.get(getVersion()),
                                         SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER,
                                         SPL30xDistribution.SPARK_VERSION));
+        result
+                .put(new NodeComponentTypeBean(ComponentType.SPARKBATCH, SparkBatchConstant.MATCH_PREDICT_COMPONENT),
+                        SPL30xNodeModuleGroup
+                                .getModuleGroup(ModuleGroupName.PARQUET.get(getVersion()),
+                                        SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER,
+                                        SPL30xDistribution.SPARK_VERSION));
 
         // Spark Batch S3
         result.put(new NodeComponentTypeBean(ComponentType.SPARKBATCH, SparkBatchConstant.S3_CONFIGURATION_COMPONENT),
